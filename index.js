d2f4fba2b80db928904e8c6861a472da44ccf824
misa vojtova <misacastella@gmail.com>
2021-11-23 19:38:43 +0000

    changed structure

--- index.js
+++ index.js
@@ -10,25 +10,7 @@ $(document).ready(function () {
 
 $(window).resize(function () {
   if ($(window).width() < 800) {
+    console.log(700);
     $nav.removeClass("collapse");
   }
 });
-
-// $(".owl-carousel").owlCarousel({
-//   loop: true,
-//   margin: 20,
-//   responsiveClass: true,
-//   autoplay: true,
-//   autoplayTimeout: 4000,
-//   responsive: {
-//     0: {
-//       items: 1,
-//     },
-//     700: {
-//       items: 1,
-//     },
-//     1000: {
-//       items: 1,
-//     },
-//   },
-// });
